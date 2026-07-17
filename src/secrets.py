02636041f3eed488bc2707452537418cd0855b23
sissygsm <trgfbvtrgfnb@gmail.com>
2026-07-17 17:18:42 -0500

    Delete src/secrets.py

--- src/secrets.py
+++
@@ -1,9 +0,0 @@
-# In Linux:
-#    Inside ~/snap/firefox/common/.mozilla/firefox/ 
-#    or ~/.mozilla/firefox/
-#
-#In Windows:
-#    In the folow place:
-#    C:\Users\your_user\AppData\Roaming\Mozilla\Firefox\Profiles\
-FIREFOX_PROFILE = 'your_profile_name' + '.default-release'
-X_USERNAME = "your_username"  # The @ username
